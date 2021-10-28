ce35a4e868bf879b4691cf62b4a8f6263c0a9459
FelipeMandelli <mandellifernandes@gmail.com>
2021-10-28 19:50:31 -0300

    Calibragem do pH

---
+++ Arduino/Teste_PH/Calibragem_PH.ino
@@ -0,0 +1,54 @@
+//----------------------------------------------------------//
+//----------------------------------------------------------//
+//  Codigo utilizado para calibrar os valores fornecidos    //
+//  pelo modulo de pH modelo Ph4502c por meio do monitor    //
+//  serial do Arduino IDE. Este codigo generico foi         //
+//  encontrado na internet e nao foi produzido pelo grupo.  //
+//  Encontrado no canal Electronic Clinic, em:              //
+//  youtube.com/watch?v=zUEl3Y3yKL4&abe e foi modificado a  //
+//  partir do codigo original disponivel no guia em:        //
+//  shorturl.at/fzDRW. Link encurtado.                      //
+//----------------------------------------------------------//
+//----------------------------------------------------------//
+
+
+float calibration_value = 21.34 - 0.3;
+int phval = 0; 
+unsigned long int avgval; 
+int buffer_arr[10],temp;
+float ph_act;
+
+  
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() {
+ for(int i=0;i<10;i++) 
+ { 
+ buffer_arr[i]=analogRead(A0);
+ delay(30);
+ }
+ for(int i=0;i<9;i++)
+ {
+ for(int j=i+1;j<10;j++)
+ {
+ if(buffer_arr[i]>buffer_arr[j])
+ {
+ temp=buffer_arr[i];
+ buffer_arr[i]=buffer_arr[j];
+ buffer_arr[j]=temp;
+ }
+ }
+ }
+ avgval=0;
+ for(int i=2;i<8;i++)
+ avgval+=buffer_arr[i];
+ float volt=(float)avgval*5.0/1024/6; 
+  ph_act = -5.70 * volt + calibration_value;
+ 
+ Serial.println("pH Val: ");
+ Serial.print(ph_act);
+ delay(1000);
+}
