d4cee742c3e1449d83c921920a4f7aa187b3c186
FelipeMandelli <mandellifernandes@gmail.com>
2021-11-18 23:45:48 -0300

    Arrumar: sensores funcionando concomitatemente

--- Arduino/Testes_Codigo/Teste_Codigo.ino
+++ Arduino/Testes_Codigo/Teste_Codigo.ino
@@ -3,166 +3,148 @@
 
 
 //Inclui Bibliotecas
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+  #include <OneWire.h>
+  #include <DallasTemperature.h>
+  #include <TinyGPS++.h>
+  #include <SoftwareSerial.h>
+
+//Define Ajuste do fuso horario ( -3 para Brasilia)
+  int fuso = -3;
 
 //Define Pinagem
 
-//Define o Pino 4 como Sensor de Temperatura
-#define ONE_WIRE_BUS 4 
+  //Define o Pino 4 como Sensor de Temperatura
+    #define ONE_WIRE_BUS 4 
 
-//
 
-//Define o Pino A0 como Sensor de pH
-const int pino_PH = A0;
+  //Define o Pino A0 como Sensor de pH
+    const int pino_PH = A0;
 
-//Define os Pinos RX e TX do GPS
-const int RXgps = 2;
-const int TXgps = 3;
+  //Define os Pinos RX e TX do GPS
+    const int RXgps = 2;
+    const int TXgps = 3;
 
-//Define Pino A1 como Sensor de Turbidez
-const int pino_Turb = A1;
+  //Define Pino A1 como Sensor de Turbidez
+    const int pino_Turb = A1;
 
 
 //Declarando Objetos 
 
-//Objetos Sensor Temperatura
-OneWire oneWire(ONE_WIRE_BUS);        
-DallasTemperature sensors(&oneWire);
-DeviceAddress sensor1;
+  //Objetos Sensor Temperatura
+  OneWire oneWire(ONE_WIRE_BUS);        
+  DallasTemperature sensors(&oneWire);
+  DeviceAddress sensor1;
 
-//Objetos GPS
-TinyGPSPlus gps;
-SoftwareSerial gpsSerial(RXgps, TXgps);
+  //Objetos GPS
+  TinyGPSPlus gps;
+  SoftwareSerial gpsSerial(RXgps, TXgps);
 
 
 //Define Baud Rate do GPS
-int gpsBaud = 9600;
+  int gpsBaud = 9600;
 
 
 //Funcoes
 
-  //Funcao para mostrar o Endereco do sensor de Temperatura
-  void mostra_endereco_sensor(DeviceAddress deviceAddress){
-    for (uint8_t i = 0; i < 8; i++){
-      //Adiciona zeros se necessário
-      if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
-    }
-  }
-
-  //Funcao para Inicializar, Obter e Mostrar o Endereco da Temperatura
-  void inicializa_sensor_temperatura() {                                                       
-    //Localiza e mostra endereco do sensor de Temperatura
-    Serial.println("Localizando sensores DS18B20...");
-    Serial.print("Foram encontrados ");
-    Serial.print(sensors.getDeviceCount(), DEC);
-    Serial.println(" sensores.");
-  
-    if (!sensors.getAddress(sensor1, 0)){
-      Serial.println("Sensores nao encontrados !");  
-    }
-      
-    //Mostra o endereco do sensor encontrado
-    Serial.print("Endereco sensor: ");
-  
-    mostra_endereco_sensor(sensor1);
- 
-    Serial.println();
-    Serial.println();
-  
-  }
-
   //Obter o Ph por Referencia (Ponteiros)
   void LerValorPH(float *pPH, bool *pStaPh){
-    //Declara Variaveis
-    float vet_precisao[10],media_prec_ph = 0, aux, conver_aux;
-    float ph_valor, calibragem = 21.34 - 0.3;
 
-    //Obtem 10 valores para media, aumentando precisao
-    for(int i = 0; i < 10; i++){
-      vet_precisao[i] = analogRead(pino_PH);
-      delay(50);
-    }
+      //Declara Variaveis
+      float vet_precisao[10],media_prec_ph = 0, aux, conver_aux;
+      float ph_valor, calibragem = 21.34 - 0.3;
 
-    //Organiza o vetor em ordem crescente
-    for (int i = 0; i < 9; i++){
-       
-      for (int j = i+1; j < 10; j++){
-       
-        if (vet_precisao[i] > vet_precisao[j]){
-          aux = vet_precisao[i];
-          vet_precisao[i] = vet_precisao[j];
-          vet_precisao[j] = aux;
+      //Obtem 10 valores para media, aumentando precisao
+      for(int i = 0; i < 10; i++){
+
+        vet_precisao[i] = analogRead(pino_PH);
+        delay(50);
+      }
+
+      //Organiza o vetor em ordem crescente
+      for (int i = 0; i < 9; i++){   
+
+        for (int j = i+1; j < 10; j++){    
+
+          if (vet_precisao[i] > vet_precisao[j]){
+
+            aux = vet_precisao[i];
+            vet_precisao[i] = vet_precisao[j];
+            vet_precisao[j] = aux;
+          }
         }
       }
-    }
 
-    //Exclui 2 maiores e 2 menores valores
-    for (int i = 2; i < 8; i++){
-      media_prec_ph += vet_precisao[i];
-    }
+      //Exclui 2 maiores e 2 menores valores
+      for (int i = 2; i < 8; i++){
 
-    //Converte para Leitura de Voltagem o valor medio
-    conver_aux = media_prec_ph / 6 * (5.0/1024.0);
+        media_prec_ph += vet_precisao[i];
+      }
 
-    //Converte para valor final do pH
-    ph_valor = -5.70 * conver_aux + calibragem;
+      //Converte para Leitura de Voltagem o valor medio
+      conver_aux = media_prec_ph / 6 * (5.0/1024.0);
 
-    //Devolve Valores de Saida
-    *pPH = ph_valor;
-    //Impede Resultado Absurdo
-    if (ph_valor >= 0 && ph_valor <= 14){
-      *pStaPh = true;
-    }
+      //Converte para valor final do pH
+      ph_valor = -5.70 * conver_aux + calibragem;
+
+      //Devolve Valores de Saida
+      *pPH = ph_valor;
+
+      //Impede Resultado Absurdo
+      if (ph_valor >= 0 && ph_valor <= 14){
+
+        *pStaPh = true;
+      }
   }
   
   // Obter a Temperatura por Referencia (Ponteiros)
   void LerValorTemperatura(float *pTemp, bool *pStaTemp){
      
      //Declaracao e Inicializa sensor
-     float vet_precisao[10], aux, media_prec_Temp = 0;
-     sensors.begin();
-     sensors.requestTemperatures();
+      float vet_precisao[10], aux, media_prec_Temp = 0;
+      sensors.begin();
+      sensors.requestTemperatures();
      
      //Obtem 10 valores para media, aumentando precisao
-     for(int i = 0; i < 10; i++){
-       vet_precisao[i] = sensors.getTempC(sensor1);
-       delay(50);
-     }
+      for(int i = 0; i < 10; i++){
+        
+        vet_precisao[i] = sensors.getTempC(sensor1);
+        delay(50);
+      }
      
      //Organiza o vetor em ordem crescente
-     for (int i = 0; i < 9; i++){
-       
-       for (int j = i+1; j < 10; j++){
-       
-         if (vet_precisao[i] > vet_precisao[j]){
-           aux = vet_precisao[i];
-           vet_precisao[i] = vet_precisao[j];
-           vet_precisao[j] = aux;
-         }
-       }
-     }
+      for (int i = 0; i < 9; i++){
+        
+        for (int j = i+1; j < 10; j++){
+        
+          if (vet_precisao[i] > vet_precisao[j]){
+
+            aux = vet_precisao[i];
+            vet_precisao[i] = vet_precisao[j];
+            vet_precisao[j] = aux;
+          }
+        }
+      }
 
     //Exclui 2 maiores e 2 menores valores
-     for (int i = 2; i < 8; i++){
-       media_prec_Temp += vet_precisao[i];
-     }
+      for (int i = 2; i < 8; i++){
+
+        media_prec_Temp += vet_precisao[i];
+      }
 
     //Devolve os Valores de Saida
-     *pTemp = media_prec_Temp/6;
-     *pStaTemp = true;
-         
+      *pTemp = media_prec_Temp/6;
+      *pStaTemp = true;
+
   }
   
   //Obter Turbiez por Referencia (Ponteiros)
   void LerValorTurbidez(int *pTurb, bool *pStaTurb){
-   int vet_precisao[10], aux, media_prec_turb = 0;
+
+    int vet_precisao[10], aux, media_prec_turb = 0;
 
     //Obtem 10 valores para media, aumentando precisao
     for(int i = 0; i < 10; i++){
+    
       vet_precisao[i] = analogRead(pino_Turb);
       delay(50);
     }
@@ -173,6 +155,7 @@ int gpsBaud = 9600;
       for (int j = i+1; j < 10; j++){
        
         if (vet_precisao[i] > vet_precisao[j]){
+    
           aux = vet_precisao[i];
           vet_precisao[i] = vet_precisao[j];
           vet_precisao[j] = aux;
@@ -182,52 +165,64 @@ int gpsBaud = 9600;
 
     //Exclui 2 maiores e 2 menores valores
     for (int i = 2; i < 8; i++){
+    
       media_prec_turb += vet_precisao[i];
     }
 
     //Devolve os Valores de Saida
     *pTurb = media_prec_turb/6;
+    
     //Impede Resultado Absurdo
     if ((media_prec_turb/6) >= 0 && (media_prec_turb/6) <= 1024){
+
       *pStaTurb = true;
     }
   }
   
   //Obter Posicao do GPS por Referencia (Ponteiros)
   void LerPosicaoGPS(float *pLat, float *pLongi, float *pAlt, bool *pStaPosGPS){
-   if (gps.location.isValid()){
-     *pLat = gps.location.lat();
-     *pLongi = gps.location.lng();
-     *pAlt = gps.altitude.meters();
-     *pStaPosGPS = true;
-   }
-   else {
-     *pStaPosGPS = false;
-   }
+
+    if (gps.location.isValid()){
+
+      *pLat = gps.location.lat();
+      *pLongi = gps.location.lng();
+      *pAlt = gps.altitude.meters();
+      *pStaPosGPS = true;
+    }
+    else {
+
+      *pStaPosGPS = false;
+    }
   }
 
   //Obter Data do GPS por Referencia (Ponteiros)
   void LerDataGPS (int *pA, int *pM, int *pD, bool *pStaDataGPS){
+
     if (gps.date.isValid()){
+
       *pA = gps.date.year();
       *pM = gps.date.month();
       *pD = gps.date.day();
       *pStaDataGPS = true;
     }
     else {
+
       *pStaDataGPS = false;
     }
   }
 
   //Obter Hora do GPS por Referencia (Ponteiros)
   void LerHoraGPS (int *pHr, int *pMin, int *pSeg, bool *pStaHoraGPS){
+
     if (gps.time.isValid()){
-      *pHr = gps.time.hour();
+
+      *pHr = gps.time.hour() + fuso; //Para fuso Horario indicado 
       *pMin = gps.time.minute();
       *pSeg = gps.time.second();
       *pStaHoraGPS = true;
     }
     else {
+
       *pStaHoraGPS = false;
     }
     
@@ -235,14 +230,15 @@ int gpsBaud = 9600;
 
 
 void setup() {
+
   //Inicia Monitor Serial
   Serial.begin(9600);
 
   //Inicializa o sensor de Temperatura
   sensors.begin();
 
-  //Chamada funcao de inicializacao da Temperatura e do Endereco do sensor
-  inicializa_sensor_temperatura();
+  //Inicializacao do Endereco do sensor de temperatura
+  sensors.getAddress(sensor1, 0);
 
   //Inicia a Porta Serial do GPS
   gpsSerial.begin(gpsBaud);
@@ -254,19 +250,19 @@ void setup() {
 
 void loop() {
 
-  //Controladores 
+  //Variaveis Controladoras 
   bool statusPh = false;
   bool statusTemperatura = false;
   bool statusTurbidez = false;  
   bool statusGPS = false;
 
   
-  //Informacoes
+  //Variaveis de informacoes
   float valorSensorPh;
   float valorSensorTemperatura;
   int valorSensorTurbidez;
   
-  //Infos do GPS
+  //Variaveis de infos do GPS
   float Latitude, Longitude, Altitude;
   int Ano, Mes, Dia;
   int Hora, Minuto, Segundo;
@@ -285,12 +281,14 @@ void loop() {
     Serial.print("Valor do sensor de Temperatura: ");
     
     if (statusTemperatura == true){
+
       Serial.print(valorSensorTemperatura);
       Serial.println ("C");
     }
     
     else {
-    Serial.println("Valor nao Obtido");
+
+      Serial.println("Valor nao Obtido");
     }
 
     Serial.println();
@@ -305,7 +303,9 @@ void loop() {
     
     //Escreve, se houver, o Valor no monitor serial
     Serial.print("Valor do sensor de pH: ");
+    
     if (statusPh == true){
+
       Serial.println(valorSensorPh);
     }
 
@@ -314,17 +314,20 @@ void loop() {
     }
 
     Serial.println();
- }
+  }
   
 
   //Obter o Valor de Turbidez
   if(statusTurbidez != true){
+
     //Chama a funcao LerValorTurbidez                  
     LerValorTurbidez(&valorSensorTurbidez, &statusTurbidez);
 
     //Escreve, se houver, o Valor no monitor serial
     Serial.print("Valor do sensor de Turbidez: ");
+
     if (statusTurbidez == true){
+
       Serial.println(valorSensorTurbidez);
     }
 
@@ -337,55 +340,82 @@ void loop() {
 
 
   if(statusGPS != true){
+    
+    //Variavel para Limitar a Espera pelos dados do GPS
+    unsigned long inicioGPS = millis();
+    
+    Serial.println("Dados do GPS: ");
+    
+    //Loop para Buscar as informacoes do GPS
+    while (statusGPS != true){
+      if (gpsSerial.available() > 0){
 
-    Serial.print("Dados do GPS: ");
+        if (gps.encode(gpsSerial.read())){
 
-    while (gpsSerial.available() > 0)
-      if (gps.encode(gpsSerial.read())){
-        //Chama a funcao LerPosicaoGPS                   
-        LerPosicaoGPS(&Latitude, &Longitude, &Altitude, &statusPosGPS);
-        //Chama a funcao LerDataGPS
-        LerDataGPS(&Ano, &Mes, &Dia, &statusDataGPS);
-        //Chama a funcao LerHoraGPS
-        LerHoraGPS(&Hora, &Minuto, &Segundo, &statusHoraGPS);
+          //Chama a funcao LerPosicaoGPS                   
+          LerPosicaoGPS(&Latitude, &Longitude, &Altitude, &statusPosGPS);
 
-        //Escreve, se houver, o Valor no monitor serial
-        if (statusPosGPS == true){
-          Serial.print("Latitude: ");
-          Serial.println(Latitude);
-          Serial.print("Longitude: ");
-          Serial.println(Longitude);
-          Serial.print("Altitude: ");
-          Serial.println(Altitude);
-          statusGPS = true;
-        }
+          //Chama a funcao LerDataGPS
+          LerDataGPS(&Ano, &Mes, &Dia, &statusDataGPS);
 
-        if (statusHoraGPS == true){
-          Serial.print("Hora: ");
-          Serial.print(Hora);
-          Serial.print(":");
-          Serial.print(Minuto);
-          Serial.print(":");
-          Serial.println(Segundo);
-          statusGPS = true;
-        }
+          //Chama a funcao LerHoraGPS
+          LerHoraGPS(&Hora, &Minuto, &Segundo, &statusHoraGPS);
 
-        if (statusDataGPS == true){
-          Serial.print("Data: ");
-          Serial.print(Dia);
-          Serial.print("/");
-          Serial.print(Mes);
-          Serial.print("/");
-          Serial.println(Ano);
-          statusGPS = true;
+          //Confere se, devido ao fuso horario necessita alteracoes na hora e dia
+          if (Hora < abs(fuso)){
+
+              Hora += 24;
+              Dia -= 1;
+            }
+  
+          //Escreve, se houver, o Valor no monitor serial
+          if (statusPosGPS == true){
+
+            Serial.print("\t\tLatitude: ");
+            Serial.println(Latitude, 6);
+            Serial.print("\t\tLongitude: ");
+            Serial.println(Longitude, 6);
+            Serial.print("\t\tAltitude: ");
+            Serial.println(Altitude);
+            statusGPS = true;
+          }
+  
+          if (statusDataGPS == true){
+
+            Serial.print("\t\tData: ");
+            Serial.print(Dia);
+            Serial.print("/");
+            Serial.print(Mes);
+            Serial.print("/");
+            Serial.println(Ano);
+            statusGPS = true;
+          }
+
+          if (statusHoraGPS == true){
+
+            Serial.print("\t\tHora: ");
+            Serial.print(Hora);
+            Serial.print(":");
+            Serial.print(Minuto);
+            Serial.print(":");
+            Serial.println(Segundo);
+            statusGPS = true;
+          }
         }
       }
 
-    else {
-      Serial.println("Dados nao Obtidos");
+      //Se espera exceder 1 segundo, sai do loop
+      if ((millis() - inicioGPS) > 1000){
+
+        Serial.println("Tempo Expirado");
+        goto controleTempo;
+      }
     }
+
+    //Rotulo para limite do tempo
+    controleTempo:
+    Serial.println();
   }
 
   delay(1000);
-
 }
